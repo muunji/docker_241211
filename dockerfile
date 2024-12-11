6e83e05f55df48237e02b410fab5878720977df0
muunji <twoji3019@naver.com>
2024-12-11 11:47:34 +0900

    docker build

---
+++ dockerfile
@@ -0,0 +1,18 @@
+# Base 이미지 설정
+FROM node:16
+
+# 컨테이너 내 앱 디렉토리 설정
+WORKDIR /app
+
+# 의존성 설치
+COPY package.json ./
+RUN yarn install
+
+# 앱 코드 복사
+COPY . .
+
+# 앱 실행 포트 설정
+EXPOSE 3050
+
+# 앱 실행 명령어
+CMD ["yarn", "start"]
---
+++ src/server.js
@@ -0,0 +1,21 @@
+const http = require('node:http');
+const fs = require('node:fs');
+const path = require('node:path');
+
+const PORT = 3050;
+const filePath = path.join(__dirname,"../public/index.html");
+
+const server = http.createServer((req,res)=>{
+  if(req.method==="GET"){
+    if(req.url==='/'){
+      const html = fs.readFileSync(filePath,'utf-8',()=>{});
+
+      res.writeHead(200,{"content-type":"text/html"})
+      res.write(html);
+      res.end();
+    }
+  }
+})
+.listen(PORT,()=>{
+  console.log(`http://localhost:${PORT}`);
+})
