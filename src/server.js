f84ea486d27a07b9a4f0a7dca295690c22820e83
muunji <twoji3019@naver.com>
2024-12-11 12:15:58 +0900

    port 변경

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const http = require('node:http');
 const fs = require('node:fs');
 const path = require('node:path');
 
-const PORT = 3050;
+const PORT = 3060;
 const filePath = path.join(__dirname,"../public/index.html");
 
 const server = http.createServer((req,res)=>{
